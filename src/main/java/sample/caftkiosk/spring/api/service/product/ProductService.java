5e87f46849a714175537de8212612fd34dd6f891
jjlim <jongjin826@gmail.com>
2024-02-16 00:04:45 +0900

    흐 뭐 공부해야될지 모르겠다 정말

--- src/main/java/sample/caftkiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/caftkiosk/spring/api/service/product/ProductService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sample.caftkiosk.spring.api.service.product.response.ProductResponse;
+import sample.caftkiosk.spring.api.service.product.request.ProductCreateRequest;
 import sample.caftkiosk.spring.domain.product.Product;
 import sample.caftkiosk.spring.domain.product.ProductRepository;
 import sample.caftkiosk.spring.domain.product.ProductSellingStatus;
@@ -15,6 +16,15 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+	// 동시성 이슈
+	// UUID ?
+	public void createProduct(ProductCreateRequest request) {
+		String nextProductNumber = createNextProductNumber();
+
+		Product product = request.toEntity(nextProductNumber);
+		Product savedProduct = productRepository.save(product);
+	}
+
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
 
@@ -22,4 +32,12 @@ public class ProductService {
             .map(ProductResponse::of)
             .collect(Collectors.toList());
     }
+
+	private String createNextProductNumber() {
+		String latestProductNumber = "";
+		if(latestProductNumber == null) {
+
+		}
+		return "";
+	}
 }
---
+++ src/test/java/sample/caftkiosk/spring/api/service/product/ProductServiceTest.java
@@ -0,0 +1,7 @@
+package sample.caftkiosk.spring.api.service.product;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+}
